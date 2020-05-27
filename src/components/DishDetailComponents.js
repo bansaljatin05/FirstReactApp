386df40705787a4abc504bba7eddeedf484fc9f6
jatin bansal <jbansal380@gmail.com>
2020-05-27 16:23:29 +0530

    redux Thunk

--- src/components/DishDetailComponents.js
+++ src/components/DishDetailComponents.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Col, Row, Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
+import {Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -138,7 +139,24 @@ const minLength = (len) => (val) => val && (val.length >= len);
         }
     }
     const DishDetails = (props) => {
-        if(props.dish != null) {
+        if(props.isLoading) {
+            return(
+                <div className="conatainer">
+                    <div className="row">
+                        <Loading/>
+                    </div>
+                </div>
+            );
+        } else if(props.errMess) {
+            return(
+                <div className="conatainer">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if(props.dish != null) {
             console.log(props.comments);
             return (
                 <div className="container">
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Comments } from './Comments';
 import { Leaders } from './Leaders';
 import { Promotions } from './Promotions';
 import { Dishes } from './Dishes';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const configureStore = () => {
     const store = createStore(
@@ -11,7 +13,8 @@ export const configureStore = () => {
             promotions: Promotions,
             leaders: Leaders,
             comments: Comments
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );  
 
     return store;
