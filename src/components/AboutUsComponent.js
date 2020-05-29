743fada18d7451330cdd7ae86b1a9e27a30a13d0
jatin bansal <jbansal380@gmail.com>
2020-05-29 14:43:24 +0530

    Asssignment 4 task 3

--- src/components/AboutUsComponent.js
+++ src/components/AboutUsComponent.js
@@ -3,6 +3,7 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderLeader({name, image, desg, desc}) {
 
@@ -27,11 +28,13 @@ function About(props) {
         leaders = (<Loading/>);
     } else if(props.leaders.errMess) leaders = (<h4>{props.leaders.errMess}</h4>)
     else {
-        leaders = props.leaders.leaders.map((leader) => {
+            leaders = props.leaders.leaders.map((leader) => {
             return (
-                <RenderLeader name={leader.name} image={baseUrl + leader.image} desg={leader.designation} desc={leader.description} />
+                <Fade in>
+                    <RenderLeader name={leader.name} image={baseUrl + leader.image} desg={leader.designation} desc={leader.description} />
+                </Fade>
             );
-        });
+        })
     }
     
 
@@ -91,7 +94,7 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list className="mt-2">
-                        {leaders}
+                        <Stagger in>{leaders}</Stagger>
                     </Media>
                 </div>
             </div>
